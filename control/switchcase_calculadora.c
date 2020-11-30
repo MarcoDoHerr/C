5bb5c5e1332000dda83834b2bd36d41a0ea1dbff
MarcoDoHerr <marco.dobladoherrera@gmail.com>
2020-11-30 13:35:16 -0600

    Factorial y funciones

--- control/switchcase_calculadora.c
+++ control/switchcase_calculadora.c
@@ -2,11 +2,54 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main()
-{
 	//Declarando variables
 	int opcion;
 	float n1, n2, resultado;
+	
+float suma(float n1, float n2){ //Implementación de función
+    float resultado = 0;
+    resultado = n1 + n2;
+    return resultado;}
+
+float resta(float n1, float n2){ //Implementación de función
+    float resultado = 0;
+    resultado = n1 - n2;
+    return resultado;}
+ 
+ float division(float n1, float n2){ //Implementación de función
+    float resultado = 0;
+    	if(n2==0){
+			printf("La operacion entre 0 no esta definida\n");
+		}else{
+		resultado = n1 / n2;}
+    return resultado;}
+
+float multiplicacion(float n1, float n2){ //Implementación de función
+    float resultado = 0;
+    resultado = n1 * n2;
+    return resultado;}
+
+float potencia(float n1, float n2){ //Implementación de función
+    float resultado = 0;
+    resultado = pow(n1,n2);
+    return resultado;}
+
+float raiz(float n1){ //Implementación de función
+    float resultado = 0;
+    resultado = sqrt(n1);
+    return resultado;}
+
+float factorial(float n1)
+{
+	if (n1 == 0) /* caso base */
+		return 1; /* como 0! = 1, regresa 1*/
+	else
+		return n1 * factorial (n1 - 1); /* llamada a esta misma función */
+}
+	   
+int main()
+{
+	
 	
 	do {
 		
@@ -18,17 +61,18 @@ int main()
 	printf("\t 4.- Multiplicacion\n");
 	printf("\t 5.- Potencia\n");
 	printf("\t 6.- Raiz cuadrada\n");
-	printf("\t 7.- Salir\n");
+	printf("\t 7.- Factorial\n");
+	printf("\t 8.- Salir\n");
 	printf("--------------------------\n");
 	scanf("%d",&opcion);
 	system("cls");
 
 	
-	if(opcion==6){
+	if(opcion==6 || opcion==7){
 		printf("\nDame el primer valor: ");
 		scanf("%f", &n1);
 	}
-	else if(opcion>=7){
+	else if(opcion>=8){
 	}
 	else{
 		printf("\nDame el primer valor: ");
@@ -37,53 +81,52 @@ int main()
 		scanf("%f", &n2);
 	}
 
-	//Estructura de control de selecciÃ³n multiple
+	//Estructura de control de selección multiple
 	switch(opcion)
 	{
 	case 1:
-		resultado = n1 + n2;
-		printf("%f", resultado);
+		//resultado = n1 + n2;
+		printf("%.03f", suma(n1,n2));
 		break;
 	case 2:
-		resultado = n1 - n2;
-		printf("%f", resultado);
+		//resultado = n1 - n2;
+		printf("%.03f", resta(n1,n2));
 		break;
 	case 3: 
-		if(n2==0)
-		{
-			printf("La operacion entre 0 no esta definida");
-		}
-		else{
-		resultado = n1 / n2;
-		printf("%f", resultado);
-		}
+		//resultado = n1 / n2;
+		printf("%.03f", division(n1,n2));
+		
 		break;
 	case 4: 
-		resultado = n1 * n2;
-		printf("%f", resultado);
+		//resultado = n1 * n2;
+		printf("%.03f", multiplicacion(n1,n2));
 		break;
 	case 5: 		
-		resultado = pow(n1,n2);
-		printf("%f", resultado);
+		//resultado = pow(n1,n2);
+		printf("%.03f", potencia(n1,n2));
 		break;
 	case 6: 
 		if(n1<0){
 				printf("No esta definida");
 		}
 		else{
-			resultado = sqrt(n1);
-			printf("%f", resultado);
+			//resultado = sqrt(n1);
+			printf("%.03f", raiz(n1));
 		}
 		break;
-	case 7:
+	case 7: 		
+		//resultado = pow(n1,n2);
+		printf("%.03f", factorial(n1));
+		break;
+	case 8:
 	printf("Vuelva Pronto\n");
 	break; 
 		default:
 		printf("Opcion Invalida\n");
 	
 	}
-	printf("Switch Finalizado\n");
+	printf("\nSwitch Finalizado\n");
 	}
-		while (opcion !=7);
-	printf("Do- While Finalizado\n");	//Sale break
+		while (opcion !=8);
+	printf("\nDo- While Finalizado\n");	//Sale break
 } 
